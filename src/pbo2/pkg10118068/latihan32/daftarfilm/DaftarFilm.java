fb4cd833c6fdb0e3881e8b9ffb6d634e635a7936
Satria Aji Putra <satriamaxt@gmail.com>
2019-10-14 13:32:15 +0700

    Initial commit

---
+++ src/pbo2/pkg10118068/latihan32/daftarfilm/DaftarFilm.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan32.daftarfilm;
+
+/**
+ *
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Membuat program untuk menampilkan daftar film
+ * berbasis object
+ */
+public class DaftarFilm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("===Daftar Film Sedang Tayang===\n");
+        
+        Film film = new Film();
+        
+        film.filmName = "Venom";
+        film.filmGenre = "Action, Horror, Scifi";
+        film.filmRating = 8.5;
+        film.filmDuration = 120;
+        film.nowPlaying();
+        
+        film.filmName = "Small Foot";
+        film.filmGenre = "Animation";
+        film.filmRating = 9;
+        film.filmDuration = 96;
+        film.nowPlaying();
+        
+        film.filmName = "Crazy Rich Asian";
+        film.filmGenre = "Comedy";
+        film.filmRating = 7.8;
+        film.filmDuration = 119;
+        film.nowPlaying();
+        
+        film.filmName = "Asih";
+        film.filmGenre = "Horror";
+        film.filmRating = 6;
+        film.filmDuration = 100;
+        film.nowPlaying();
+        
+    }
+    
+}
